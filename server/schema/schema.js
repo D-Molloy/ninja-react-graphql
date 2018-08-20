f9a8094ee29b802e5ec718bbfe1ebe0b7e1188af
Denis Molloy <denis.molloy@gmail.com>
2018-08-20 16:07:18 -0700

    updated BookType with author type relation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,24 +9,72 @@ const {
     GraphQLObjectType, 
     GraphQLString, 
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt
 } = graphql;
 
 // DUMMY DATA
 const books = [
-    {name: 'The Demon Haunted World', genre: 'Self Help', id: '1'},
-    {name: 'The Shining', genre: 'Horror', id: '2'},
-    {name: 'Dr. Sleep', genre: 'Horror', id: '3'}
+    {name: 'Cosmos', genre: 'Self Help', id: '1', authorId:"1"},
+    {name: 'The Demon Haunted World', genre: 'Self Help', id: '2', authorId:"1"},
+    {name: 'Pale Blue Dot', genre: 'Self Help', id: '3', authorId:"1"},
+    {name: 'The Shining', genre: 'Horror', id: '4', authorId:"2"},
+    {name: 'Dr. Sleep', genre: 'Horror', id: '5', authorId:"2"},
+    {name: 'Rules for a Knight', genre: 'Fantasy', id: '6', authorId:"3"}
+]
+
+const authors =[
+    {name: 'Carl Sagan', age: 'Dead', id:'1' },
+    {name: 'Stephen King', age:'60' , id:'2' },
+    {name: 'Ethan Hawke', age:'45' , id:'3' }
 ]
 
 //fields needs to be a function is that types will have references to another, and the function allows universal knowledge of others
-//
+// when we have type relations (e.g. every book has an author and every author has books) the books need to be able to bounce back and force with the functions
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: {type: GraphQLID},
         name:{type: GraphQLString},
-        genre:{type: GraphQLString}
+        genre:{type: GraphQLString},
+        //linking book to author
+        // parent param comes into play the first time
+        author:{
+            type: AuthorType,
+            resolve(parent, args){
+                // 1 - console.log below
+                console.log(parent)
+                //find the author whose id matches parent.authorId
+                return _.find(authors,{id: parent.authorId})
+            }
+        }
+    })
+});
+
+// 1 - example query
+// {
+//     book(id: 1) {
+//       name
+//       genre
+//       author {
+//         name
+//       }
+//     }
+//   }
+
+// logs
+// { name: 'The Demon Haunted World',
+//   genre: 'Self Help',
+//   id: '1',
+//   authorId: '1' }
+
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name:{type: GraphQLString},
+        age:{type: GraphQLInt}
     })
 });
 
@@ -45,6 +93,13 @@ const RootQuery = new GraphQLObjectType({
                 //code to get data from db/other source
                 //finding book with the id
                return _.find(books, {id: args.id})
+            }
+        },
+        author:{
+            type: AuthorType,
+            args:{id:{type: GraphQLID}},
+            resolve(parents, args){
+                return _.find(authors, {id: args.id})
             }
         }
     }
